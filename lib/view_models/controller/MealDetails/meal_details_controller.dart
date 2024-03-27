c1dcb41051cfb80d1be558bf7ca6d4acf8fa49ae
tanzir.ratul <tanzir.ratul@gmail.com>
2024-03-27 08:27:48 +0600

    sqlite helper class add

--- lib/view_models/controller/MealDetails/meal_details_controller.dart
+++ lib/view_models/controller/MealDetails/meal_details_controller.dart
@@ -1,9 +1,27 @@
-
-
 import 'package:get/get.dart';
+import 'package:meal/models/dbModels/meals_details_db_model.dart';
+import 'package:meal/repository/mealRepositoryDB/meal_repository_db.dart';
 
-class MealDetailsController extends GetxService{
+class MealDetailsController extends GetxService {
+  final MealRepository repository = MealRepository();
 
+  RxList<MealsDetails> mealsDetailsList = <MealsDetails>[].obs;
+  Future<bool> addMeals(MealsDetails mealsDetails) async {
+    try {
+      return await repository.insertMealDetails(mealsDetails);
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
 
-
+  Future<List<MealsDetails>?> getAllMealDetails() async {
+    try {
+      mealsDetailsList.value =  (await repository.getAllMealDetails())!;
+      return mealsDetailsList;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
 }
